1ac6407c1a221c3e76600108598aa90d1739bc61
JosehpCR <josephcamilo2002@gmail.com>
2025-07-10 15:03:13 -0500

    package.json actualizado

--- frontend/src/components/Providers.tsx
+++ frontend/src/components/Providers.tsx
@@ -4,13 +4,12 @@ import { ReactNode } from 'react'
 import { NextUIProvider } from '@nextui-org/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { SocketToast } from '@/components/feedback/Toast'
-import { theme } from '@/styles/themes'
 
 const queryClient = new QueryClient()
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <NextUIProvider theme={theme}>
+    <NextUIProvider>
       <QueryClientProvider client={queryClient}>
         {children}
         <SocketToast />
