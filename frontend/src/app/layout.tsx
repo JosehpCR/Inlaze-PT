e609cfc6bc8e9bea91b540823af384a7e5906578
Josehp Camilo Ramos Novoa <109875273+JosehpCR@users.noreply.github.com>
2025-07-10 13:23:32 -0500

    fix: wrap app with client providers

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { NextUIProvider } from '@nextui-org/react';
-import { theme } from '@/styles/themes';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { SocketToast } from '@/components/feedback/Toast';
 import { ReactNode } from 'react';
+import Providers from './providers';
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
 const geistMono = Geist_Mono({ variable: '--font-geist-mono', subsets: ['latin'] });
@@ -15,18 +12,11 @@ export const metadata: Metadata = {
   description: 'Dashboard',
 };
 
-const queryClient = new QueryClient();
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <QueryClientProvider client={queryClient}>
-          <NextUIProvider theme={theme}>
-            {children}
-            <SocketToast />
-          </NextUIProvider>
-        </QueryClientProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
